6987d1900ba6ba3c3be455a04eedc7fdf0d80050
Wesley Reisz <wes@wesleyreisz.com>
2013-06-30 11:46:28 -0400

    added custom view rather than using defaultt

--- src/com/wesleyreisz/android/criminalintent/CrimeListFragment.java
+++ src/com/wesleyreisz/android/criminalintent/CrimeListFragment.java
@@ -2,15 +2,18 @@ package com.wesleyreisz.android.criminalintent;
 
 import java.util.List;
 
-import com.wesleyreisz.android.criminalintent.model.Crime;
-
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.wesleyreisz.android.criminalintent.model.Crime;
 
 public class CrimeListFragment extends ListFragment {
 	private static final String TAG = "CrimeListFragment";
@@ -22,9 +25,7 @@ public class CrimeListFragment extends ListFragment {
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();
 		
 		ArrayAdapter<Crime> adapter =
-				new ArrayAdapter<Crime>(getActivity(),
-						android.R.layout.simple_list_item_1,
-						mCrimes);
+				new CrimeAdapter(mCrimes);
 		
 		setListAdapter(adapter);
 	}
@@ -32,10 +33,37 @@ public class CrimeListFragment extends ListFragment {
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
-		Crime c = (Crime)(getListAdapter().getItem(position));
+		Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 		Log.d(TAG,c.getTitle() + " was Clicked");
 		
 		Toast toast = Toast.makeText(getActivity(),c.getTitle() + " was Clicked",Toast.LENGTH_SHORT);
 		toast.show();
+	}
+	
+	private class CrimeAdapter extends ArrayAdapter<Crime>{
+		public CrimeAdapter(List<Crime>crimes){
+			super(getActivity(),0,crimes);
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView==null){
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_layout, null);
+			}
+			
+			Crime c = getItem(position);
+			TextView titleTextView = 
+					(TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+			titleTextView.setText(c.getTitle());
+			
+			TextView dateTextView = 
+					(TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+			dateTextView.setText(c.getDate().toString());
+			
+			CheckBox solvedCheckBox =
+					(CheckBox)convertView.findViewById(R.id.crime_list_item_solved);
+			solvedCheckBox.setChecked(c.isSolved());
+			
+			return convertView;
+		}
 	}
 }
